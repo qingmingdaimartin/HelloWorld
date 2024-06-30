2716dda82631b71156b91b2103763d4402ba33b9
qingmingdaimartin <qingmingdaimartin@gmail.com>
2024-06-30 19:39:07 +1000

    gitignore

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync(__dirname + '/../data/db.json');
+const db = low(adapter)
+
+const shortid = require('shortid');
+
 // accounts list
 router.get('/account', function(req, res, next) {
   res.render('list');
@@ -12,7 +20,8 @@ router.get('/account/create', function(req, res, next) {
 });
 
 router.post('/account', function(req, res) {
-  console.log(req.body)
+  let id = shortid.generate();
+  db.get('accounts').unshift({id:id, ...req.body}).write();
   res.send('add record');
 });
 
