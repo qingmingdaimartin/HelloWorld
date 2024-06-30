e4021a59146510370b59f22d9dc380df9fa4f84b
qingmingdaimartin <qingmingdaimartin@gmail.com>
2024-06-30 17:55:45 +1000

    llowdb

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -11,4 +11,9 @@ router.get('/account/create', function(req, res, next) {
   res.render('create');
 });
 
+router.post('/account', function(req, res) {
+  console.log(req.body)
+  res.send('add record');
+});
+
 module.exports = router;
