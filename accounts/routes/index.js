91047c312c30e01c5fa5f667011cb954aec409fd
qingmingdaimartin <qingmingdaimartin@gmail.com>
2024-06-30 17:36:41 +1000

    atatic web

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// accounts list
+router.get('/account', function(req, res, next) {
+  res.render('list');
+});
+
+// add record
+router.get('/account/create', function(req, res, next) {
+  res.render('create');
 });
 
 module.exports = router;
